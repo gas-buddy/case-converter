7511f8c9e89c8d039001b8eb1f5eaebb574d69fb
Chris Johnson <cjohnson@gasbuddy.com>
2019-04-05 08:02:43 -0600

    first commit

---
+++ Case/Case.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case
+{
+    public class Converter
+    {
+        public static string ToPascal(string orig)
+        {
+            if (Snake.IsSnakeCase(orig))
+            {
+                var snake = new Snake();
+                return snake.ToPascal(orig);
+            }
+
+            return orig;
+        }
+    }
+}
---
+++ CaseTests/SnakeTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CaseTests
+{
+    [TestFixture]
+    public class SnakeTests
+    {
+        [TestCase("asdf_sf", true)]
+        [TestCase("asdf_sf_sdf", true)]
+        [TestCase("asdfsf_", true)]
+        [TestCase("_asdfsf", true)]
+        public void SnakeCaseDetection(string input, bool expected)
+        {
+            var result = Case.Snake.IsSnakeCase(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("this_is_snake_case", "ThisIsSnakeCase")]
+        [TestCase("thisislikeonewordinsnakecase", "Thisislikeonewordinsnakecase")]
+        [TestCase("PascalCase", "Pascalcase")]
+        [TestCase("_weird_case_", "WeirdCase")]
+        public void SnakeConversion(string input, string expected)
+        {
+            var snake = new Case.Snake();
+            var result = snake.ToPascal(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
